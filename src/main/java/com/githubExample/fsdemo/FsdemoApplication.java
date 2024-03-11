2a6dc4ccba7e340fcc88cc1710b0ef06f06d1ad3
kantsr <kantharajasr5@gmail.com>
2024-03-11 11:36:32 +0530

    initial demo

---
+++ src/main/java/com/githubExample/fsdemo/FsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.githubExample.fsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubExample/fsdemo/FsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubExample.fsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
